95cafb2f29d0d0dabb58abedc88134765eca4b73
GustavoLuna87 <gustavo.luna87@hotmail.com>
2024-03-13 10:59:58 -0300

    atualização no JS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 const botao = document.querySelector(".buttonResult");
 const input = document.querySelector(".resultadoSorteado")
 
-botao.addEventListener('click', function (){
+botao.addEventListener('click', function () {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min +1)) + min;
+    if (min >= max) {
+        alert("O valor mínimo deve ser menor que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     input.value = result
 })
